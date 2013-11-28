888bd59ecf63e2e5ceaa2b290c8de50040e97101
Gkowloon <coulombgun@yahoo.co.jp>
2013-11-28 15:59:47 +0900

    プレイヤークラスの変更

---
+++ Raysist/Class1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DxLibDLL;
+
+namespace Raysist
+{
+    class ShotComponent : GameComponent
+    {
+        //---------------------------------------------------
+        //メンバ変数こっから
+
+        /// <summary>
+        /// @brief 体力
+        /// </summary>
+        public float Angle
+        {
+            private set;
+            get;
+        }
+
+
+        //ここまで
+        //----------------------------------------------------
+
+        /// <summary>
+        /// @brief コンストラクタ
+        /// </summary>
+        public ShotComponent(GameContainer container, float angle, Vector3 position)
+            : base(container)
+        {
+            Position.LocalRotation *= new Quaternion(Vector3.AxisY, (float)Math.PI);
+            Position.LocalPosition = position;
+            Angle = angle;
+        }
+
+        /// <summary>
+        /// @brief 更新
+        /// </summary>
+        public override void Update()
+        {
+            // 移動処理
+
+            Position.LocalPosition.x += (float)Math.Cos(Angle) * 10.0f;
+            Position.LocalPosition.z += (float)Math.Sin(Angle) * 10.0f;
+        }
+
+
+    }
+}
--- Raysist/Player.cs
+++ Raysist/Player.cs
@@ -6,6 +6,24 @@ namespace Raysist
 {
     class Player : GameComponent
     {
+        //---------------------------------------------------
+        //メンバ変数こっから
+
+        /// <summary>
+        /// @brief 体力
+        /// </summary>
+        public int Life
+        {
+            private set;
+            get;
+        }
+
+
+        //ここまで
+        //----------------------------------------------------
+
+
+
         /// <summary>
         /// @brief コンストラクタ
         /// </summary>
@@ -39,19 +57,29 @@ namespace Raysist
             {
                 Position.LocalPosition.x += 1.0f;
             }
+
+            if (DX.CheckHitKey(DX.KEY_INPUT_SPACE) == 1)
+            {
+                
+            }
         }
     }
 
     class Bit : GameComponent
     {
-        public Bit(GameContainer container) : base(container)
+        public Bit(GameContainer container, Vector3 position) : base(container)
         {
-            Position.LocalPosition = new Vector3 { x = 40.0f, y = 0.0f, z = -40.0f };
+            Position.LocalPosition = position;
         }
 
         public override void Update()
         {
-            
+            var c = new ContainerFactory((GameContainer g) =>
+            {
+                g.AddComponent(new ShotComponent(g, (float)Math.PI / 2.0f, Position.WorldPosition));
+                g.AddComponent(new MeshRenderer(g, "fighter.x"));
+            });
+            c.Create();
         }
     }
 
@@ -68,6 +96,7 @@ namespace Raysist
 
         public override void EnterScene()
         {
+
             var cf = new ContainerFactory((GameContainer g) =>
             {
                 g.AddComponent(new Player(g));
@@ -83,19 +112,32 @@ namespace Raysist
             var gc = cf.Create(Root);
             var player = gc.GetComponent<Player>();
 
+
             var bitmaker = new ContainerFactory((GameContainer g) =>
             {
-                g.AddComponent(new Bit(g));
+                g.AddComponent(new Bit(g, new Vector3 { x = 40.0f, y = 0.0f, z = -40.0f }));
 
                 var br = new BillboardRenderer(g, "");
                 var animator = new Animator(g, br, "explosion.png", 4, 4, 1, 512, 512);
                 br.Scale *= 25.0f;
                 animator.UpdateFrame = 5;
                 g.AddComponent(br);
-                g.AddComponent(animator); 
+                g.AddComponent(animator);
             });
 
+            var bit2maker = new ContainerFactory((GameContainer g) =>
+            {
+                g.AddComponent(new Bit(g, new Vector3 { x = -40.0f, y = 0.0f, z = -40.0f }));
+
+                var br = new BillboardRenderer(g, "");
+                var animator = new Animator(g, br, "explosion.png", 4, 4, 1, 512, 512);
+                br.Scale *= 25.0f;
+                animator.UpdateFrame = 5;
+                g.AddComponent(br);
+                g.AddComponent(animator);
+            });
             bitmaker.Create(gc);
+            bit2maker.Create(gc);
             //player.Position.LocalRotation = new Quaternion(Vector3.AxisX, (float)Math.PI * 0.5f) * new Quaternion(Vector3.AxisZ, (float)Math.PI);
 
             var camera = cameraFactory.Create(Root).GetComponent<Camera>();
