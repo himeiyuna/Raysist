fdca19daa2faea74265c2a8dd087aef1ba50e1c6
Gkowloon <coulombgun@yahoo.co.jp>
2014-02-01 23:11:59 +0900

    追加修正
    Objも見るように　あとスティックの対応

--- Raysist/purge.bat
+++ Raysist/purge.bat
@@ -3,6 +3,10 @@ del /q bin\Debug\Raysist.exe
 del /q bin\Debug\Raysist.pdb
 del /q bin\Release\Raysist.exe
 del /q bin\Release\Raysist.pdb
+del /q obj\Debug\Raysist.exe
+del /q obj\Debug\Raysist.pdb
+del /q obj\Release\Raysist.exe
+del /q obj\Release\Raysist.pdb
 del /q bin\Debug\Log.txt
 del /q obj\Debug\DesignTimeResolveAssemblyReferencesInput.cache
 del /q bin\Release\Log.txt
